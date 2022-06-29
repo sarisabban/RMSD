5042ff0aca8a033a9bf1799534707697e476fbb9
Sari Sabban <sari.sabban@gmail.com>
2022-06-29 04:03:56 +0300

    Update RMSD.py

--- RMSD.py
+++ RMSD.py
@@ -1,22 +1,22 @@
 import Bio.PDB
 
-def RMSD (structure_1, structure_2):
+def RMSD(structure_1, structure_2):
 	''' Calculate the RMSD between two protein structures '''
-	type1 = Bio.PDB.Polypeptide.PPBuilder().build_peptides(Bio.PDB.PDBParser(QUIET=True).get_structure('X', structure_1), aa_only=True)
-	length1 = type1[-1][-1].get_full_id()[3][1]
-	fixed = [atom['CA'] for atom in type1[0]]
-	#Second structure
-	type2 = Bio.PDB.Polypeptide.PPBuilder().build_peptides(Bio.PDB.PDBParser(QUIET=True).get_structure('X', structure_2), aa_only=True)
-	length2 = type2[-1][-1].get_full_id()[3][1]
-	moving = [atom['CA'] for atom in type2[0]]
-	#Choose the length of the smallest structure
-	lengths = [length1, length2]
-	smallest = min(int(item) for item in lengths)
-	#Find RMSD
+	builder = Bio.PDB.Polypeptide.PPBuilder()
+	STR1 = builder.build_peptides(Bio.PDB.PDBParser(QUIET=True)\
+		.get_structure('Structure 1', structure_1), aa_only=True)
+	STR2 = builder.build_peptides(Bio.PDB.PDBParser(QUIET=True)\
+		.get_structure('Structure 2', structure_2), aa_only=True)
+	fixed  = [atom['CA'] for poly in STR1 for atom in poly]
+	moving = [atom['CA'] for poly in STR2 for atom in poly]
+	lengths = [len(fixed), len(moving)]
+	print(lengths)
+	smallest = min(lengths)
 	sup = Bio.PDB.Superimposer()
 	sup.set_atoms(fixed[:smallest], moving[:smallest])
-	sup.apply(Bio.PDB.PDBParser(QUIET=True).get_structure('X', structure_2)[0].get_atoms())
+	sup.apply(Bio.PDB.PDBParser(QUIET=True)\
+		.get_structure('Structure 2', structure_2)[0].get_atoms())
 	RMSD = round(sup.rms, 4)
 	print(RMSD)
 
-RMSD('6ooy.pdb', '6ooy.pdb')
+RMSD('6ooy.pdb', '6ooz.pdb')
